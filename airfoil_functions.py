2f903e5d547037cb0bccccc133dfb8ac8ff253d6
Seb <sebastien.bouttefeux@student.uclouvain.be>
2024-04-02 10:02:19 +0200

    global structure

--- airfoil_functions.py
+++ airfoil_functions.py
@@ -11,6 +11,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
+class Panel():
+    def __init__(self, x_start, y_start, x_end, y_end):
+        self.start = (x_start, y_start)
+        self.end = (x_end, y_end)
+        self.control = (1/2 * (x_start+x_end), 1/2 * (y_start+y_end))
+        self.length = (np.abs(x_end-x_start), np.abs(y_end-y_start))
+        
 def thickness(x, a, b, c, d, e):
     y = (a*np.sqrt(x) + b*x + c*x**2 + d*x**3 + e*x**4)
     return y
@@ -32,10 +39,21 @@ def get_intra_extra(x, c, airfoil):
         
     return y_intra, y_extra
 
+def influence_coefficients(panel_i, panel_j):
+    # Calculate the influence of the panel j on the panel i
+    
+    if (i==j):
+                # deuxième partie des formules
+            else:
+                # première partie des formules
+    return 0
+
 if __name__ == "__main__":
     airfoil_data = np.loadtxt('Airfoil-RevE-HC.dat')
     c = 2
     N = 200
+    AoA = 5
+    
     # Create x discretization with a change of variable to get more points on LE and TE
     theta = np.linspace(0, np.pi, N)
     x_c = c*(0.5)*(1-np.cos(theta))
@@ -51,6 +69,16 @@ if __name__ == "__main__":
     
     ml, t = c*mean_line(x_c/c, 0.00215, 0.47815, -0.87927, 0.68242, -0.28378), c*thickness(x_c/c, 0.40182, -0.10894, -0.60082, 0.2902, 0.01863)
     
+    ### Setup of the influence matrix and the RHS
+    b = np.zeros(N)
+    A = np.zeros((N,N))
+    
+    for i in range(N-1):          # Vérifier le N-1, mais je crois qu'on peut juste l'imposer avec la condition au TE
+        panel_i = Panel(x[i], airfoil_fun[i], x[i+1], airfoil_fun[i+1])
+        for j in range(N-1):
+            panel_j = Panel(x[j], airfoil_fun[j], x[j+1], airfoil_fun[j+1])
+            A[i,j] = influence_coefficients(panel_i, panel_j)
+            
     ### Plot airfoil
     fig,ax = plt.subplots()
     
